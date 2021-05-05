2a6d6a929a541c91119e4712ea6f175ff640101c
Matthew Fuller <63671581+mdfull01@users.noreply.github.com>
2021-05-05 17:59:15 -0400

    create index.html and main.js

---
+++ Basic_JavaScript_Projects/Project1_Expressions_Alert/JS/main.js
@@ -0,0 +1,17 @@
+document.write("Hello, world!");
+
+var A="This is a string."; //Defining variable A and assinging a string value
+window.alert(A); //Using the window.alert method to create a pop-up message
+
+document.write("\"This is in quotes\""); //using the escape character \ to make " a string
+document.write("this string"+" and this string"+" are concantenated"); //concantenating strings
+var B= "concantenated"+" string"; // assigning concantenated strings to a variable
+document.write(B);
+
+var entree="Taco", appetizer="Chips", drink="Soda"; // assigning multiple variables at once
+document.write(appetizer);
+var entree=entree.fontcolor("green");
+var appetizer=appetizer.fontcolor("red");
+var drink=drink.fontcolor("blue");
+document.write(appetizer);
+document.write(3+3);
