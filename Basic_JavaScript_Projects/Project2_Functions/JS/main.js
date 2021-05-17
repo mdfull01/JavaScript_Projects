9b6c4e3c9d2c144ce3b423b848ad6a40e1b0a9d2
Matthew Fuller <63671581+mdfull01@users.noreply.github.com>
2021-05-17 14:29:15 -0400

    create index.html and main.js

--- Basic_JavaScript_Projects/Project2_Functions/JS/main.js
+++ Basic_JavaScript_Projects/Project2_Functions/JS/main.js
@@ -1,9 +1,5 @@
 
-
-
-function updateName() { //declare function
-var firstName="Matt ", lastName="Fuller"; //declare variables
-firstName += lastName; //concantenate both variables containing strings
-document.getElementById("area").innerHTML=firstName; //find element with id=area and replace its content with the results of updateName
+function myFunction(a, b) {
+    return a*b;
 }
-updateName();
+var x=myFunction(5, 6);
