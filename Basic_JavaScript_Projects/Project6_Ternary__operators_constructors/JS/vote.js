48163d315ea902128966157e7e554a0c93dd410b
Matt Fuller <63671581+mdfull01@users.noreply.github.com>
2022-06-21 20:34:09 -0400

    Delete Basic_JavaScript_Projects directory

--- Basic_JavaScript_Projects/Project6_Ternary__operators_constructors/JS/vote.js
+++
@@ -1,6 +0,0 @@
-function vote_Function() {
-    var Age, can_Vote;
-    Age = document.getElementById("Age").value;
-    Can_vote = (Age < 18) ? "You are not old enough":"You are old enough";
-    document.getElementById("Vote").innerHTML = Can_vote + " to vote.";
-}
